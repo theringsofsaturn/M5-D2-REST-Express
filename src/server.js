d33e48af71ee1a6423b19a40fe9ed615baae94da
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-19 19:42:54 +0200

    server added

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-import express from "express"
+import express from "express";
+
+const server = express();
+
+server.use(express.json());
+// If I do NOT specify this line BEFORE the endpoints all the requests' bodies will be UNDEFINED
+
+// ************* ENDPOINTS *****************
+
+server.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
