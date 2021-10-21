fd211c8f6cc21f9b0974634b86d82607c9c78300
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-21 16:31:01 +0200

    validation updated

---
+++ src/services/blog/validation.js
@@ -0,0 +1,8 @@
+import { body } from "express-validator"
+
+
+export const blogValidation = [
+  body("category").exists().withMessage("Category is a mandatory field!"),
+  body("title").exists().withMessage("Title is a mandatory field!"),
+  body("content").exists().withMessage("Content is a mandatory field!")
+]
